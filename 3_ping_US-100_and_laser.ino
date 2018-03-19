8d004263d7743aeb5cb0122fc7466ee86349cc4f
Thomas Weeks <tweeks-homework@theweeks.org>
2018-03-19 10:39:40 -0400

    Updated file name, corrected typos.

--- 3_ping_US-100_and_laser.ino
+++ 3_ping_US-100_and_laser.ino
@@ -1,16 +1,16 @@
-// ping_US-100_sensor
-// for Let's Code Blacksburg! www.letscodeblacksburg.org 2015-04-28
+// 3_ping_US-100_and_laser
+// for Let's Code Blacksburg! www.letscodeblacksburg.org 2018-03-19
 // By Thomas "Tweeks" Tweeks, tweeks-homework(at)theweeks.org
 //
-// This ping-sensor code is set up for using a four pin ping sensor such as
-// the US-100 module:
+// This ping-sensor & laser code is set up for using a four pin ping sensor such as
+// the US-100 or HC-SR04 ultrasonic distance sensor module:
 // https://www.bananarobotics.com/shop/US-100-Ultrasonic-Distance-Sensor-Module
 // http://www.iseerobot.com/produk-1255-us100-ultrasonic-sensor-for-arduino.html
 // NOTE: For this code, + the US-100 module, be sure to remove the serial-data jumper on the
 // back so that you get back the raw pulse from the echo pin (and not serial distance data).
 //
-// CAUTION: Have your code checked, and have the TA RUN IT BEFORE hooking up the US-100 ping
-//          sensor! Not doing so could damage the Arduino!
+// CAUTION: Have your code checked, and have the TA RUN IT BEFORE connecting the US-100 ping
+//          sensor! Not doing so could damage the Arduino and/or sensor!
 // This is set up to direct connect power and ground for the US-100 ping sensor to be
 // on pins 10(5v) 11(ping) 12(echo) 13(low/gnd) 14(hard ground)
 
@@ -21,8 +21,8 @@ const int gnd1Pin = 13;     // simulated ground so we can safely plug the module
 //last GND pin goes to "pin 14" on the arduino or sensor shield
 
 // Here is where we hook up the 5mW laser diode. We provide power and ground from the digital output pins.
-const int laserPin = 4;      // Pin the laser power is on
-const int laserGnd = 2;      // Pin the laser ground in on
+const int laserPin = 4;      // the pin the laser's power pin goes to
+const int laserGnd = 2;      // the pin the laser's ground pin goes to
 
 
 long dist = 0;
@@ -37,20 +37,19 @@ void setup() {
   // on pins:                       
   // US-100  \--5v-trg-echo-GND-GND--/
   //             |   |   |   |   |
-  // Arduino     10  11  12  13  14(hard ground)
+  // Arduino     10  11  12  13  14(hard ground, if present)
   //    (looking from the outter edge of arduino pin header)
   pinMode(gnd1Pin, OUTPUT);      // sets up ping module's inner GND pin on a low output, and
   digitalWrite(gnd1Pin, LOW);     // the outter GND pin to hard GND (on most arduinos)
   pinMode(powerPin, OUTPUT);
   digitalWrite(powerPin, HIGH);  // try to power the module from pin
-  // ** US-100 PING SENSOR I/O PINS
+  // ** US-100 ping sensor I/O pin setup
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   pinMode(echoPin, OUTPUT);    // just to make sure
   digitalWrite(echoPin, LOW);  // we clear any previous settings
   pinMode(echoPin, INPUT);     // and then use it as INPUT
   delay(500);
-
   // Laser Setup
   pinMode(laserGnd, OUTPUT);    // Laser GND pin
   digitalWrite(laserGnd, LOW);
@@ -58,11 +57,7 @@ void setup() {
   digitalWrite(laserPin, HIGH);  // Test pulse laser
   delay(250);                   // for 1/4 second
   digitalWrite(laserPin, LOW);  // Start off with laser off
-  
-
-
-
-
+  // Set up serial port (for communcating back to the PC)
   Serial.begin(9600);
 }
 
@@ -70,7 +65,7 @@ void setup() {
 
 
 // ***********************************************************
-// ***** fireLaser() *********************************************
+// ***** fireLaser() *****************************************
 // ***********************************************************
 void fireLaser(int count){
     int x=0;
@@ -93,14 +88,12 @@ void fireLaser(int count){
 // Runs forever...
 void loop() {
 
-  dist = getdist();              // looks wit hping sensor to measure distance to any objects
+  dist = getdist();              // looks with ping sensor to measure distance to any objects
   Serial.print("Distance = ");
   Serial.println(dist);
   delay(100);
 
-
   fireLaser(1);	// fire the impressive 5,000 microWatt 650nM laser cannon z times
-
 }
 
 // ***********************************************************
